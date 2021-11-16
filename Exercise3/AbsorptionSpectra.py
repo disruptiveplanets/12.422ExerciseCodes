8946b440e458c48f14c57c8de8ea6a6e7233a629
prajwal309 <prajwalniraula@gmail.com>
2021-11-16 11:14:38 -0500

    Monor changes.

--- Exercise3/AbsorptionSpectra.py
+++ Exercise3/AbsorptionSpectra.py
@@ -11,7 +11,7 @@ hapi.db_begin('data') #Load everything in the data website
 
 #Let's make some absortion cross-section
 nu_Voigt, coef_Voigt = hapi.absorptionCoefficient_Voigt(SourceTables='CO2', Diluent={'air':1}, \
-				Environment={'p':1.0, 'T':296}, OmegaStep=0.01)
+				Environment={'p':1.0, 'T':296}, OmegaStep=0.01, HITRAN_units=False)
 
 #Convert the wavenumbers to microns
 WavelengthGrid = 1/nu_Voigt*1e4
@@ -21,7 +21,7 @@ WavelengthGrid = 1/nu_Voigt*1e4
 plt.figure(figsize=(12,7))
 plt.plot(WavelengthGrid[::-1], coef_Voigt[::-1], "k-")
 plt.xlabel("Wavelength (microns)", fontsize=20)
-plt.ylabel("Absortion Cross-section \n (cm-2/molecule)", fontsize=20)
+plt.ylabel("Absortion Cross-section \n (cm-1)", fontsize=20)
 plt.xscale('log')
 plt.yscale('log')
 plt.tight_layout()
--- Exercise3/ReadMe.md
+++ Exercise3/ReadMe.md
@@ -1,3 +1,13 @@
-This exercise is to create absortion cross-section of CO from using hitran linelists. The cross-section for CO has already been downloaded using hapi.
+
+
+This exercise is to create absortion cross-section of CO2 from using hitran linelists.
+
+Download.py downloads the cross section.
+
+Make a folder data--- mkdir data
+
+and copy CO2.header and CO2.data into this folder. Alread done
+
+Now run AbsorptionSpectra.py
 
 Try using different line profile to see how they are different from one another.
