e9027a23d5b935e851683d9b350f486bbde33f38
prajwal309 <prajwalniraula@gmail.com>
2021-11-17 17:02:23 -0500

    Finalized Changes

--- Exercise3/Download.py
+++ Exercise3/Download.py
@@ -1,10 +1,11 @@
 from hapi import *
 
-#Suspected molecules in Proxima cen b
 
 #Let's download into data folder
-fetch('data/CO2',2,1,1000, 10000) #Download from 1 microns to 10 micron range. 1000 per cm = 10 microns and 10000 per cm is 1 microns
-fetch('data/H2O',1,1, 1000, 10000)
-fetch('data/CO',5,1, 1000, 10000)
-fetch('data/NH3',11,1, 1000, 10000)
-fetch('data/PH3',28,1, 1000, 10000)
+#fetch('data/H2O',1,1, 1000, 10000)#Download from 1 microns to 10 micron range. 1000 per cm = 10 microns and 10000 per cm is 1 microns
+fetch('data/CO2',2,1,1000, 10000)
+#fetch('data/CO',5,1, 1000, 10000)
+#fetch('data/O2',7,1, 1000, 10000)
+#fetch('data/NH3',11,1, 1000, 10000)
+#fetch('data/N2',22,1, 1000, 10000)
+#fetch('data/PH3',28,1, 1000, 10000)
