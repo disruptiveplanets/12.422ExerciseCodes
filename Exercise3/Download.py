63d8419ae0f2e5abfa41f7af49f66120ecd06dfb
prajwal309 <prajwalniraula@gmail.com>
2021-11-17 09:24:39 -0500

    Finalized for PSET

--- Exercise3/Download.py
+++ Exercise3/Download.py
@@ -6,6 +6,5 @@ from hapi import *
 fetch('data/CO2',2,1,1000, 10000) #Download from 1 microns to 10 micron range. 1000 per cm = 10 microns and 10000 per cm is 1 microns
 fetch('data/H2O',1,1, 1000, 10000)
 fetch('data/CO',5,1, 1000, 10000)
-fetch('data/CH4',6,1, 1000, 10000)
 fetch('data/NH3',11,1, 1000, 10000)
 fetch('data/PH3',28,1, 1000, 10000)
